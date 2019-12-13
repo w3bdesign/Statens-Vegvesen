0bee9e4d1a17bfb80da3129dcfc34f1f869deea6
w3bdesign <hijack_xxx@yahoo.com>
2019-12-13 03:55:08 +0100

    Small cleanup in code

--- js/statens-vegvesen.js
+++ js/statens-vegvesen.js
@@ -1,17 +1,3 @@
-function checkValidText() {
-  const bilInformasjon = document.getElementById("bilinformasjon").value;
-  const letters = /[A-Z]{2}[0-9]{5}/gi;
-  if (
-    bilInformasjon.match(letters) &&
-    bilInformasjon &&
-    bilInformasjon.length === 7
-  ) {
-    document.getElementById("submitButton").removeAttribute("disabled");
-  } else {
-    document.getElementById("submitButton").setAttribute("disabled", true);
-  }
-}
-
 function sendForm() {
   // https://jsonplaceholder.typicode.com/posts
   const bilInformasjon = document.getElementById("bilinformasjon").value;
