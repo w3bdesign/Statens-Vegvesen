7b4098b17f9a58b963795cd9eeb9ca0e202d7592
w3bdesign <hijack_xxx@yahoo.com>
2019-12-14 01:55:26 +0100

    Improve error handling
    Added some default values in case the data is null, added better comments for the HTML and added  more informative errors. Also, we won't show the information table if we encounter an error.

--- js/statens-vegvesen.js
+++ js/statens-vegvesen.js
@@ -1,20 +1,34 @@
 function sendForm() {
-  // https://jsonplaceholder.typicode.com/posts
   const bilInformasjon = document.getElementById("bilinformasjon").value;
-  //const regNummer = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${bilInformasjon}`;
-  //const regNummer = `http://localhost:8080/bil/${bilInformasjon}`;
-
   const regNummer = `https://statens-vegvesen-express.herokuapp.com/bil/${bilInformasjon}`;
-
   // Show loading spinner
   document.getElementById("loadingSpinner").classList.remove("hide");
-
   fetch(regNummer)
     .then(async response => {
       const text = await response.text();
       const informasjonBil = JSON.parse(text);
+
+      // Set some default values in case they are null (no information is registered)
+      // We do not want to use || as it may introduce some bugs
+      if (informasjonBil.kjennemerke === null) {
+        informasjonBil.kjennemerke = "(Ingen informasjon registrert)";
+      }
+      if (informasjonBil.registrering.forstegangsregistrering === null) {
+        informasjonBil.registrering.forstegangsregistrering =
+          "(Ingen informasjon registrert)";
+      }
+      if (informasjonBil.registrering.forstegangsregistreringEier === null) {
+        informasjonBil.registrering.forstegangsregistreringEier =
+          "(Ingen informasjon registrert)";
+      }
+      if (informasjonBil.periodiskKjoretoykontroll.sistKontrollert === null) {
+        informasjonBil.periodiskKjoretoykontroll.sistKontrollert =
+          "(Ingen informasjon registrert)";
+      }
+
       // Hide loading spinner
       document.getElementById("loadingSpinner").classList.add("hide");
+      // Dont show information table
       document.getElementById("tableElement").classList.remove("scale-out");
 
       document.getElementById(
@@ -31,13 +45,24 @@ function sendForm() {
       ).innerHTML = `<td>${informasjonBil.periodiskKjoretoykontroll.sistKontrollert}</td>`;
     })
     .catch(function(error) {
-      // If there is any error you will catch them here
+      // Convert error message to string so we can compare it
+      const errorToString = error.toString();
+
+      // TODO Replace with a switch case ?
+      if (
+        errorToString === "TypeError: informasjonBil.registrering is undefined"
+      ) {
+        error =
+          "Registreringsnummeret eksisterer ikke i databasen. Vennligst prøv et annet nummer.";
+      }
+
+      // Dont show table if there is an error
+      document.getElementById("tableElement").classList.add("scale-out");
       // Hide loading spinner
       document.getElementById("loadingSpinner").classList.add("hide");
       document.getElementById(
         "feilMelding"
       ).innerHTML = `En feil har oppstått under henting av informasjon.
-        <br/>Feilmelding: 
-        ${error}`;
+        <br/> ${error}`;
     });
 }
