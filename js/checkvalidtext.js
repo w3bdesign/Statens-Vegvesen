843355b3c6bbd088f0283db06b59c6cae982d8c1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-07-16 02:17:35 +0200

    Prettier

--- js/checkvalidtext.js
+++ js/checkvalidtext.js
@@ -1,13 +1,13 @@
 function checkValidText() {
-  const bilInformasjon = document.getElementById("bilinformasjon").value;
+  const bilInformasjon = document.getElementById('bilinformasjon').value;
   const letters = /[A-Z]{2}[0-9]{5}/gi;
   if (
     bilInformasjon.match(letters) &&
     bilInformasjon &&
     bilInformasjon.length === 7
   ) {
-    document.getElementById("submitButton").removeAttribute("disabled");
+    document.getElementById('submitButton').removeAttribute('disabled');
   } else {
-    document.getElementById("submitButton").setAttribute("disabled", true);
+    document.getElementById('submitButton').setAttribute('disabled', true);
   }
 }
--- js/statens-vegvesen.js
+++ js/statens-vegvesen.js
@@ -1,69 +1,68 @@
 function sendForm() {
-  const bilInformasjon = document.getElementById("bilinformasjon").value;
+  const bilInformasjon = document.getElementById('bilinformasjon').value;
   const regNummer = `https://statens-vegvesen-express.herokuapp.com/bil/${bilInformasjon}`;
   // Hide error message when try to fetch a new number
-  document.getElementById("feilMelding").innerHTML = ``;
+  document.getElementById('feilMelding').innerHTML = ``;
   // Show loading spinner
-  document.getElementById("loadingSpinner").classList.remove("hide");
+  document.getElementById('loadingSpinner').classList.remove('hide');
   fetch(regNummer)
-    .then(async response => {
+    .then(async (response) => {
       const text = await response.text();
       const informasjonBil = JSON.parse(text);
-
       // Set some default values in case they are null (no information is registered)
       // We do not want to use || as it may introduce some bugs
       if (informasjonBil.kjennemerke === null) {
-        informasjonBil.kjennemerke = "(Ingen informasjon registrert)";
+        informasjonBil.kjennemerke = '(Ingen informasjon registrert)';
       }
       if (informasjonBil.registrering.forstegangsregistrering === null) {
         informasjonBil.registrering.forstegangsregistrering =
-          "(Ingen informasjon registrert)";
+          '(Ingen informasjon registrert)';
       }
       if (informasjonBil.registrering.forstegangsregistreringEier === null) {
         informasjonBil.registrering.forstegangsregistreringEier =
-          "(Ingen informasjon registrert)";
+          '(Ingen informasjon registrert)';
       }
       if (informasjonBil.periodiskKjoretoykontroll.sistKontrollert === null) {
         informasjonBil.periodiskKjoretoykontroll.sistKontrollert =
-          "(Ingen informasjon registrert)";
+          '(Ingen informasjon registrert)';
       }
 
       // Hide loading spinner
-      document.getElementById("loadingSpinner").classList.add("hide");
+      document.getElementById('loadingSpinner').classList.add('hide');
       // Dont show information table
-      document.getElementById("tableElement").classList.remove("scale-out");
+      document.getElementById('tableElement').classList.remove('scale-out');
 
       document.getElementById(
-        "kjenne"
+        'kjenne'
       ).innerHTML = `<td>${informasjonBil.kjennemerke}</td>`;
       document.getElementById(
-        "forstenorge"
+        'forstenorge'
       ).innerHTML = `<td>${informasjonBil.registrering.forstegangsregistrering}</td>`;
       document.getElementById(
-        "forsteeier"
+        'forsteeier'
       ).innerHTML = `<td>${informasjonBil.registrering.forstegangsregistreringEier}</td>`;
       document.getElementById(
-        "periodiskKjoretoykontroll"
+        'periodiskKjoretoykontroll'
       ).innerHTML = `<td>${informasjonBil.periodiskKjoretoykontroll.sistKontrollert}</td>`;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Convert error message to string so we can compare it
       const errorToString = error.toString();
 
       // TODO Replace with a switch case ?
       if (
-        errorToString === "TypeError: informasjonBil.registrering is undefined"
+        errorToString === 'TypeError: informasjonBil.registrering is undefined'
       ) {
         error =
-          "Registreringsnummeret eksisterer ikke i databasen. Vennligst prøv et annet nummer.";
+          'Registreringsnummeret eksisterer ikke i databasen. Vennligst prøv et annet nummer.';
       }
 
       // Dont show table if there is an error
-      document.getElementById("tableElement").classList.add("scale-out");
+      document.getElementById('tableElement').classList.add('scale-out');
       // Hide loading spinner
-      document.getElementById("loadingSpinner").classList.add("hide");
+      document.getElementById('loadingSpinner').classList.add('hide');
       document.getElementById(
-        "feilMelding"
+        'feilMelding'
       ).innerHTML = `En feil har oppstått under henting av informasjon.
         <br/> ${error}`;
     });
