9c46606b652c04515ec6353c5991e91661c57312
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-25 22:05:12 +0200

    Show error

--- scripts/classes/CheckValidText.ts
+++ scripts/classes/CheckValidText.ts
@@ -1,7 +1,5 @@
 export default class CheckValidText {
   public checkValidText(event: Event) {
-    console.log('Checking text ...');
-
     const bilInformasjon = (<HTMLInputElement>event.target).value;
     const submitButton = window.document.getElementById('submitButton');
     const letters = /[A-Z]{2}[0-9]{5}/gi;
--- scripts/classes/SendForm.ts
+++ scripts/classes/SendForm.ts
@@ -6,7 +6,7 @@ export default class SendForm {
     SendForm.bilInformasjon = (<HTMLInputElement>(
       window.document.getElementById('bilinformasjon')
     )).value;
-    SendForm.regNummer = `https://statens-vegvesen-express.vercel.app/bil/${SendForm.bilInformasjon}`;
+    SendForm.regNummer = `https://statens-vegvesen-express.vercel.appx/bil/${SendForm.bilInformasjon}`;
   }
 
   public sendForm(e: Event) {
@@ -21,10 +21,48 @@ export default class SendForm {
   }
 
   static fetchRemoteData() {
-    fetch(SendForm.regNummer).then(async (response) => {
-      const text = await response.text();
-      const informasjonBil = JSON.parse(text);
-      console.log(informasjonBil);
-    });
+    fetch(SendForm.regNummer)
+      .then(async (response) => {
+        const text = await response.text();
+        const informasjonBil = JSON.parse(text);
+
+        // We have an error
+        if (informasjonBil.melding) {
+          window.document.getElementById('feilMelding')!.innerHTML =
+            informasjonBil.melding;
+          window.document
+            .getElementById('loadingSpinner')!
+            .classList.add('hide');
+        }
+      })
+      .catch(function (error) {
+        window.document.getElementById('loadingSpinner')!.classList.add('hide');
+
+        window.document.getElementById('feilMelding')!.innerHTML =
+          'En feil har oppstått, vennligst prøv igjen.';
+      });
+  }
+
+  static setDefaults(informasjonBil: any): any {
+    if (informasjonBil === undefined) {
+      informasjonBil = '(Ingen informasjon om kjøretøy er registrert)';
+    }
+
+    if (informasjonBil.kjennemerke === null) {
+      informasjonBil.kjennemerke = '(Ingen informasjon registrert)';
+    }
+    if (informasjonBil.registrering.forstegangsregistrering === null) {
+      informasjonBil.registrering.forstegangsregistrering =
+        '(Ingen informasjon registrert)';
+    }
+    if (informasjonBil.registrering.forstegangsregistreringEier === null) {
+      informasjonBil.registrering.forstegangsregistreringEier =
+        '(Ingen informasjon registrert)';
+    }
+    if (informasjonBil.periodiskKjoretoykontroll.sistKontrollert === null) {
+      informasjonBil.periodiskKjoretoykontroll.sistKontrollert =
+        '(Ingen informasjon registrert)';
+    }
+    return informasjonBil;
   }
 }
