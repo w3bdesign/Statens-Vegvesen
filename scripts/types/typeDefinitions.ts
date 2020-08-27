32c5a92213c902d66ad1d4e879b04f828eeb2965
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-27 22:18:34 +0200

    Prettier

--- scripts/types/typeDefinitions.ts
+++ scripts/types/typeDefinitions.ts
@@ -1,21 +1,21 @@
 /**
- * Type definition for data returned from API. 
+ * Type definition for data returned from API.
  * Returns an object with strings.
  */
 export type TStatensVegvesenBilData = {
-    // Will return error message if the car was not found
-    melding: string;
-    /**
-     * Registration number for personalised number plate)
-     * @see https://www.vegvesen.no/en/vehicles/own-and-maintain/number-plates/personalised-number-plates
-     */
-    kjennemerke: string;
-    registrering: {
-      // Details about when car was registered for the first time
-      forstegangsregistrering: string;
-      // Details about when car was registered on owner
-      forstegangsregistreringEier: string;
-    };
-    // Last time the car had a mandatory roadworthiness test
-    periodiskKjoretoykontroll: { sistKontrollert: string };
+  // Will return error message if the car was not found
+  melding: string;
+  /**
+   * Registration number for personalised number plate)
+   * @see https://www.vegvesen.no/en/vehicles/own-and-maintain/number-plates/personalised-number-plates
+   */
+  kjennemerke: string;
+  registrering: {
+    // Details about when car was registered for the first time
+    forstegangsregistrering: string;
+    // Details about when car was registered on owner
+    forstegangsregistreringEier: string;
   };
+  // Last time the car had a mandatory roadworthiness test
+  periodiskKjoretoykontroll: { sistKontrollert: string };
+};
