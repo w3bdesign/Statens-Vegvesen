42572dd716bb43c8814922d87ab8b195883f7f3c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-02-16 17:44:05 +0100

    Update classValidText.ts

--- scripts/classes/ValidText/classValidText.ts
+++ scripts/classes/ValidText/classValidText.ts
@@ -8,7 +8,7 @@ export default class classValidText {
    * @param {Event} event Used to fetch the value from the text input
    */
   public checkValidText(event: Event) {
-    // Need to cast this to <HTMLInputElement> or Typescript gives us an error
+    // Need to cast this as <HTMLInputElement> or Typescript gives us an error
     const bilInformasjon = (<HTMLInputElement>event.target).value;
     const submitButton = window.document.getElementById("submitButton");
     const letters = /[A-Z]{2}[0-9]{5}/gi;
