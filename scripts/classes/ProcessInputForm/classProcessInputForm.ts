911f539ee18ad9c2e634e690949663aaea72bc23
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-03 22:51:36 +0100

    REfactor

--- scripts/classes/ProcessInputForm/classProcessInputForm.ts
+++ scripts/classes/ProcessInputForm/classProcessInputForm.ts
@@ -5,6 +5,7 @@ import type { IStatensVegvesenBilData } from "../../types/typeDefinitions";
 
 let remoteBilData: IStatensVegvesenBilData;
 
+// Helper function to set innerHTML
 const setInnerHTML = (elementId: string, value: string) => {
   const element = window.document.getElementById(elementId);
   if (element && value) {
@@ -12,6 +13,9 @@ const setInnerHTML = (elementId: string, value: string) => {
   }
 };
 
+/**
+ * Add remote data to the table
+ */
 const addDataToTable = () => {
   setInnerHTML("kjennemerke", remoteBilData.kjennemerke);
   setInnerHTML(
@@ -25,28 +29,42 @@ const addDataToTable = () => {
   setInnerHTML("sistKontrollert", remoteBilData.sistKontrollert);
 };
 
+// This function processes remote data
 const processRemoteData = () => {
+  // Check if the remote data has a 'melding' property
   if (remoteBilData.melding !== undefined) {
+    // If it does, display an error message using the 'classErrorHandler' object and hide the data table
     classErrorHandler.displayErrorFromAPI(remoteBilData);
     classShowHideElements.hideDataTable();
+
     return;
   }
+  // If the remote data does not have a 'melding' property, hide the loading spinner and show the data table
   classShowHideElements.hideLoadingSpinner();
   classShowHideElements.showDataTable();
+  // Add the remote data to the table
   addDataToTable();
+  // Reset any error text using the 'classErrorHandler' object
   classErrorHandler.resetErrorText();
 };
 
+// This function is called when a form is submitted
 const sendForm = (event: Event): void => {
+  // Prevent the default form submission behavior
   event.preventDefault();
+  // Show the loading spinner using the 'classShowHideElements' object
   classShowHideElements.showLoadingSpinner();
+  // Fetch remote data using the 'classFetchRemoteData' object
   classFetchRemoteData
     .fetchRemoteData()
     .then((response) => {
+      // If the fetch is successful, store the response in the 'remoteBilData' variable
       remoteBilData = response;
+      // Call the 'processRemoteData' function to handle the data
       processRemoteData();
     })
     .catch(() => {
+      // If the fetch fails, do nothing and return from the function early
       return;
     });
 };
