908e08c16c0fe19e0fd0aad408d9bc46f0407b7a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-08 23:07:32 +0200

    Test nytt api

--- api/backup-getRegNummer.ts
+++ api/backup-getRegNummer.ts
@@ -1,28 +1,23 @@
 import axios from "axios";
 import { sanitize } from "isomorphic-dompurify";
-import { NextApiRequest, NextApiResponse } from "next";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { IStatensVegvesenFullData } from "../scripts/types/typeDefinitions";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export default async function getRegNummer(
+  req: VercelRequest,
+  res: VercelResponse
 ): Promise<void> {
   const { regNummer = "" } = req.query;
 
   if (regNummer === undefined) {
-    res.status(400).json({ error: "Mangler regNummer parameter" });
+    res.send({ error: "Mangler regNummer parameter" });
     return;
   }
 
-  const urlToFetch = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoyinfo/kjop/sok-kjennemerke?p_registreringsnummer=${regNummer}`;
+  const urlToFetch = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${regNummer}`;
 
   try {
-    const response = await axios.get<IStatensVegvesenFullData>(urlToFetch, {
-      headers: {
-        "SVV-Authorization": `Apikey ${process.env.SVV_API_KEY}`,
-        "X-Client-Identifier": "my-app",
-      },
-    });
+    const response = await axios.get<IStatensVegvesenFullData>(urlToFetch);
 
     if (response.status === 200) {
       const {
@@ -38,12 +33,12 @@ export default async function handler(
         sistKontrollert: sanitize(sistKontrollert),
       };
 
-      res.status(200).json(sanitizedData);
+      res.send(sanitizedData);
     } else {
-      res.status(500).json({ error: "Feil under henting av data" });
+      res.send({ error: "Feil under henting av data" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Feil under henting av data" });
+    res.send({ error: "Feil under henting av data" });
   }
 }
