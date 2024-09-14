ba24ca03109b0c8b5c0e51d8dc66c51280b02a59
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-09-14 07:21:41 +0200

    Update getRegNummer.ts

--- api/getRegNummer.ts
+++ api/getRegNummer.ts
@@ -26,7 +26,7 @@ export default async function handler(
       },
     });
 
-    console.log(JSON.stringify(response.data));
+    // console.log(JSON.stringify(response.data));
 
     if (response.status === 200) {
       const {
