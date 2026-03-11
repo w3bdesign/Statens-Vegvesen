fbd278e8e3842bff09f6f3278c106eb0fbadf1d5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2026-03-12 00:04:27 +0100

    Refactor

--- api/utils/builders/buildMotorOgYtelse.ts
+++ api/utils/builders/buildMotorOgYtelse.ts
@@ -1,4 +1,4 @@
-import { safe, sanitizeNum, sanitizeBool, sanitizeKode } from "../sanitize";
+import { buildSection, FieldDescriptor } from "./buildFields";
 import type {
   IMotorOgYtelse,
   Motor,
@@ -8,6 +8,34 @@ import type {
   ForbrukOgUtslipp,
 } from "../../../scripts/types/typeDefinitions";
 
+/** Source bundle passed into each field accessor */
+interface MotorOgYtelseSources {
+  motor: Motor | null;
+  motorOgDrivverk: MotorOgDrivverk | null;
+  miljodata: Miljodata | null;
+  miljoGruppe: MiljoOgdrivstoffGruppe | null;
+  forbruk: ForbrukOgUtslipp | null;
+}
+
+/** Declarative field definitions for the Motor & Ytelse section */
+const motorOgYtelseFields: ReadonlyArray<FieldDescriptor<MotorOgYtelseSources>> = [
+  { key: "drivstofftype", type: "kode", get: (s) => s.miljoGruppe?.drivstoffKodeMiljodata },
+  { key: "motoreffektKw", type: "num", get: (s) => s.motor?.drivstoff?.[0]?.maksNettoEffekt },
+  { key: "slagvolumCc", type: "num", get: (s) => s.motor?.slagvolum },
+  { key: "antallSylindre", type: "num", get: (s) => s.motor?.antallSylindre },
+  { key: "girkassetype", type: "kode", get: (s) => s.motorOgDrivverk?.girkassetype },
+  { key: "antallGir", type: "num", get: (s) => s.motorOgDrivverk?.antallGir },
+  { key: "hybridKategori", type: "kode", get: (s) => s.motorOgDrivverk?.hybridKategori },
+  { key: "maksHastighetKmT", type: "num", get: (s) => s.motorOgDrivverk?.maksimumHastighet?.[0] },
+  { key: "euroKlasse", type: "kode", get: (s) => s.miljodata?.euroKlasse },
+  { key: "co2BlandetKjoring", type: "num", get: (s) => s.forbruk?.co2BlandetKjoring },
+  { key: "forbrukBlandetKjoring", type: "num", get: (s) => s.forbruk?.forbrukBlandetKjoring },
+  { key: "noxUtslippMgKm", type: "num", get: (s) => s.forbruk?.utslippNOxMgPrKm },
+  { key: "partikkelfilter", type: "bool", get: (s) => s.forbruk?.partikkelfilterFabrikkmontert },
+  { key: "rekkeviddeKm", type: "num", get: (s) => s.forbruk?.rekkeviddeKm },
+  { key: "stoynivaaDb", type: "num", get: (s) => s.miljoGruppe?.lyd?.standstoy },
+];
+
 /** Build the Motor & Ytelse section of the vehicle data response */
 export function buildMotorOgYtelse(
   motor: Motor | null,
@@ -16,31 +44,8 @@ export function buildMotorOgYtelse(
   miljoGruppe: MiljoOgdrivstoffGruppe | null,
   forbruk: ForbrukOgUtslipp | null,
 ): IMotorOgYtelse {
-  return {
-    drivstofftype: sanitizeKode(
-      safe(() => miljoGruppe?.drivstoffKodeMiljodata),
-    ),
-    motoreffektKw: sanitizeNum(
-      safe(() => motor?.drivstoff?.[0]?.maksNettoEffekt),
-    ),
-    slagvolumCc: sanitizeNum(safe(() => motor?.slagvolum)),
-    antallSylindre: sanitizeNum(safe(() => motor?.antallSylindre)),
-    girkassetype: sanitizeKode(safe(() => motorOgDrivverk?.girkassetype)),
-    antallGir: sanitizeNum(safe(() => motorOgDrivverk?.antallGir)),
-    hybridKategori: sanitizeKode(safe(() => motorOgDrivverk?.hybridKategori)),
-    maksHastighetKmT: sanitizeNum(
-      safe(() => motorOgDrivverk?.maksimumHastighet?.[0]),
-    ),
-    euroKlasse: sanitizeKode(safe(() => miljodata?.euroKlasse)),
-    co2BlandetKjoring: sanitizeNum(safe(() => forbruk?.co2BlandetKjoring)),
-    forbrukBlandetKjoring: sanitizeNum(
-      safe(() => forbruk?.forbrukBlandetKjoring),
-    ),
-    noxUtslippMgKm: sanitizeNum(safe(() => forbruk?.utslippNOxMgPrKm)),
-    partikkelfilter: sanitizeBool(
-      safe(() => forbruk?.partikkelfilterFabrikkmontert),
-    ),
-    rekkeviddeKm: sanitizeNum(safe(() => forbruk?.rekkeviddeKm)),
-    stoynivaaDb: sanitizeNum(safe(() => miljoGruppe?.lyd?.standstoy)),
-  };
+  return buildSection<MotorOgYtelseSources, IMotorOgYtelse>(
+    { motor, motorOgDrivverk, miljodata, miljoGruppe, forbruk },
+    motorOgYtelseFields,
+  );
 }
